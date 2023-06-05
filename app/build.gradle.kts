6936031f4751fd2e67631db9ec02b00d0e2743ad
Patryk Mis <foss@patrykmis.com>
2023-06-05 23:27:56 +0200

    build: updates
    * Update JAVA to 17
    * Update Kotlin to v1.8.22
    * Update Android Gradle Plugin to v8.0.2
    * update Material library to v1.9.0
    * Update AndroidX dependencies
    * Update Gradle to v8.2
    
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ val versionPatch = 2 // 0..9
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -94,12 +94,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         buildConfig = true
@@ -107,9 +107,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Unit test
