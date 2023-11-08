9051e7c41efcafa50f7727c4fac3d58daab5ca47
Patryk Miś <foss@patrykmis.com>
2023-11-08 18:53:06 +0100

    Add CheckedTextView support
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- app/src/main/java/com/neo/speaktouch/utils/Reader.kt
+++ app/src/main/java/com/neo/speaktouch/utils/Reader.kt
@@ -148,6 +148,14 @@ class Reader @Inject constructor(
             Type.OPTIONS -> context.getString(R.string.text_options_type)
             Type.LIST -> context.getString(R.string.text_list_type)
             Type.TITLE -> context.getString(R.string.text_title_type)
+            Type.CHECKEDTEXT -> {
+                val selectionStatus = if (node.isChecked) {
+                    node.stateDescription?.toString() ?: context.getString(R.string.text_selected)
+                } else {
+                    ""
+                }
+                "$selectionStatus"
+            }
         }
     }
 
