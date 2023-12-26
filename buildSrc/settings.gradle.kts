3b2be3a6f08ba0dbeb4ed149159511ca0dbfedd9
Irineu333 <mmmirineusilva@gmail.com>
2023-12-26 15:00:22 -0300

    fix(build): no locally installed toolchains match and toolchain download repositories have not been configured.

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -29,3 +29,7 @@ dependencyResolutionManagement {
         }
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,9 +34,7 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-}
+
 
 rootProject.name = "SpeakTouch"
 
